520544f9a2320a19234d1bc48a66943b97399b2a
Chris Yunbin Chang <hw@yunbin.kr>
2019-10-30 01:54:52 +0900

    delete not erased area.

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ function check_new_post() {
               })
             } isLatest = false;
 
-    
+
             if(idx > before_latest) {
               subject = $(this).find('td.subject > a');
               title = subject.text();
@@ -112,45 +112,7 @@ function check_new_post() {
   )
 
   await browser.close();
-
-
-  request('http://www.hansung.ac.kr/web/www/1323', function(err, res, body) {
-    if(err) throw err;
-    console.log(getDate(), 'Start Checking : ' + res.statusCode);
-    $ = cheerio.load(body);
-
-    isLatest = true;
-
-    tr = $('tr')
-
-    tr.each(function(index, element) {
-
-      idx = $(this).find('td')
-      .first()
-      .text()
-      .trim();// Get a each post-id from the webpage.
-      console.log("idx", idx)
-      if(idx != '') {
-        if(isLatest) {
-          latest = idx;
-          fs.writeFile('./storage/latest.txt', latest, 'utf8', function(err) {
-            if(err) throw err;
-          })
-        } isLatest = false;
-
-
-        if(idx > before_latest) {
-          subject = $(this).find('td.subject > a');
-          title = subject.text();
-          link = subject.attr('href');
-          console.log(getDate(), idx, title, link);
-          mailer.sendNotification(title, link);
-
-        } //new post will be handled here.
-      }// filter new post
-
-    })
-    console.log(getDate(), 'End Checking.')
+  console.log(getDate(), 'End Checking.')
   });
 
 }
