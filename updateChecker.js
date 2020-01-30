1502a7095ec0440c5c5149d6566e02e871030364
techbless <hw@yunbin.kr>
2020-01-30 20:21:52 +0900

    Fix deployment issues.

--- updateChecker.js
+++ updateChecker.js
@@ -6,18 +6,22 @@ const URL = 'http://www.hansung.ac.kr/web/www/1323';
 
 function checkNewPost() {
   console.log(getDate(), 'Start Checking.');
-  puppeteer.launch({ headless: true, args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox'
-    ]}).then(async browser => {
-    const page = await browser.newPage();
-    await page.setViewport({width: 320, height: 600})
-    await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 9_0_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13A404 Safari/601.1')
+  puppeteer.launch({args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox'
+  ]}).then(async browser => {
+    let page = undefined;
+    try {
+      page = await browser.newPage();
+      await page.setViewport({width: 320, height: 600})
+      await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 9_0_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13A404 Safari/601.1')
 
-    await page.goto(URL, {waitUntil: 'networkidle0'});
-    await page.waitForSelector('tr');
-    await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'});
-    //await page.addScriptTag({content: 'fs'})
+      await page.goto(URL, {waitUntil: 'networkidle0'});
+      await page.waitForSelector('tr');
+      await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'});
+    } catch (e) {
+      console.log(e);
+    }
 
     const result = await page.evaluate(() => {
       let posts = [];
