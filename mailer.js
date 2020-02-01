641763c0f024681263cf6d240336dd6553a2692e
techbless <hw@yunbin.kr>
2020-02-01 15:41:29 +0900

    Fix typo

--- mailer.js
+++ mailer.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const smtpPool = require('nodemailer-smtp-pool');
 const storage = require('./storage');
 
-console.log(process.env.MAIL_USER, process.env.MYSQL_PASSWORD); //test deployment setting
+console.log(process.env.MAIL_USER, process.env.MAIL_PASSWORD); //test deployment setting
 
 const transporter = nodemailer.createTransport(smtpPool({
   service: 'Gmail',
