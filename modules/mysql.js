839dfa660b6874f05db5cc0cbc40e977b901a6da
techbless <hw@yunbin.kr>
2020-02-23 23:06:09 +0900

    Install mysql and Add moudle

---
+++ modules/mysql.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql');
+const { promisify } = require('util');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  connectionLimit: 10
+});
+
+const _query = promisify(pool.query).bind(pool);
+
+function getConnection() {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, conn) => {
+      if(err) {
+        reject(err);
+      } else {
+        resolve(conn);
+      }
+    });
+  });
+}
+
+module.exports = {
+  _query: _query,
+  getConnection: getConnection
+};
