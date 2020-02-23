44cef3a32cea5ab9ec7203250be0ffeb52ecebcf
techbless <hw@yunbin.kr>
2020-02-23 23:10:17 +0900

    Deploy unsubscription into real service

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/register/email/', function(req, res) {
     });
 });
 
-app.get('/verify/email/:email/:token', function (req, res) {
+app.get('/verify/subscribe/:email/:token', function (req, res) {
   const email = req.params.email;
   const token = req.params.token;
 
@@ -64,6 +64,7 @@ app.get('/verify/email/:email/:token', function (req, res) {
               title: '구독 성공',
               content: '비교과 공지 알림 구독을 성공적으로 마쳤습니다. 이제 새로운 공지를 편하게 확인하세요.'
             });
+            storage.updateToken(email, null);
             mailer.sendWelcomeMail([email]);
           } else {
             res.render('result', {
@@ -94,7 +95,76 @@ app.get('/unsubscribe', function(req, res) {
 });
 
 app.post("/unsubscribe", function(req, res) {
-  res.send("done");
+  const email = req.body.email;
+
+  subscribe.requestUnsubscription(email)
+    .then((success) => {
+      if(success) {
+        res.render('result', {
+          title: '메일 인증',
+          content: '인증메일이 발송되었습니다. 메일 인증을 마치면 구독취소가 완료됩니다.'
+        })
+      } else {
+        res.render('result', {
+          title: '등록되지 않은 메일주소 입니다.',
+          content: '등록되지 않은 메일주소 입니다. 다시 확인해주세요.'
+        });
+      }
+
+    })
+    .catch((err) => {
+      res.render('result', {
+        title: '메일 전송 실패',
+        content: '인증메일 전송에 실패했습니다.'
+      });
+      console.log(err);
+    });
+});
+
+app.get('/verify/unsubscribe/:email/:token', function(req, res) {
+  const email = req.params.email;
+  const token = req.params.token;
+
+  subscribe.verifyEmail(email, token)
+    .then(verified => {
+      if (!verified) {
+        res.render('result', {
+          title: '유효하지 않은 메일인증',
+          content: '유효하지 않은 메일 인증입니다.'
+        });
+        return;
+      }
+
+      subscribe.unsubscribe(email)
+        .then(success => {
+          if(success) {
+            res.render('result', {
+              title: '구독 취소 완료',
+              content: '비교과 공지 알림 구독을 성공적으로 취소했습니다. 더 이상 새로운 공지 알림을 받을 수 없습니다.'
+            });
+          } else {
+            res.render('result', {
+              title: '구독 취소 실패',
+              content: '구독된 이메일이 아니거나 잘못된 이메일 주소입니다.'
+            });
+          }
+        })
+        .catch(err => {
+          res.render('result', {
+            title: '구독 취소 실패',
+            content: '구독자 리스트에서 삭제하는 동안 오류가 발생했습니다.'
+          });
+          console.log(err);
+        });
+
+    })
+    .catch((err) => {
+      res.render('result', {
+        title: '메일 인증 실패',
+        content: '메일 인증 과정에서 오류가 발생했습니다.'
+      });
+      console.log(err);
+    })
 });
 
 app.listen(process.env.PORT, function() {
