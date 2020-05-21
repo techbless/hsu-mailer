9f5f9fb68f436e631f23498b50c2758336d6782f
techbless <yunbin@hansung.ac.kr>
2020-05-21 19:32:34 +0900

    Seperate verify function for subscribe and unsubscribe

--- src/services/token.ts
+++ src/services/token.ts
@@ -1,16 +1,45 @@
 import * as crypto from 'crypto';
-import Token from '../models/token';
+import Token, { Purpose } from '../models/token';
+
+import SubscriptionService from './subscription';
 
 class TokenService {
-  public async issueToken(emailId: number) {
+  public async issueToken(subscriberId: number, purposeOrNull: Purpose) {
     const token = crypto.randomBytes(64).toString('hex');
 
     const issuedToken = await Token.create({
       token,
-      emailId,
+      subscriberId,
+      purpose: purposeOrNull,
     });
 
     return issuedToken.token;
+  }
+
+  public async verifyToken(email: string, tokenFromLink: string, purpose: Purpose) {
+    const subscriber = await SubscriptionService.findSubscriberByAddress(email);
+
+    if (!subscriber) {
+      return false;
+    }
+
+    const token = await subscriber.getTokens({
+      where: {
+        token: tokenFromLink,
+        purpose,
+      },
+    });
+
+    // if the token exists here, the verification is valid.
+    if (token.length <= 0) {
+      return false;
+    }
+
+    token[0].destroy();
+    subscriber.isVerified = true;
+    subscriber.save();
+
+    return true;
   }
 }
 
