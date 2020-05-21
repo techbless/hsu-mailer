f71f30eae1fcfe5d6f0feed2b6fe1aebb1e1b389
techbless <yunbin@hansung.ac.kr>
2020-05-21 19:29:25 +0900

    Add Subscriber model

---
+++ src/models/subscriber.ts
@@ -0,0 +1,48 @@
+import { Model, DataTypes, HasManyGetAssociationsMixin } from 'sequelize';
+import { sequelize } from './sequelize';
+import { dbType } from './index';
+
+import Token from './token';
+
+class Subscriber extends Model {
+    public readonly subscriberId!: number;
+
+    public email!: string;
+
+    public isVerified!: boolean;
+
+    public readonly createdAt!: Date;
+
+    public readonly updatedAt!: Date;
+
+    public getTokens!: HasManyGetAssociationsMixin<Token>;
+}
+
+Subscriber.init({
+  subscriberId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    unique: true,
+  },
+  isVerified: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  modelName: 'Subscriber',
+  tableName: 'subscribers',
+  charset: 'utf8mb4',
+  collate: 'utf8mb4_unicode_ci',
+});
+
+export const associate = (db: dbType) => {
+  Subscriber.hasMany(db.Token, { foreignKey: 'subscriberId' });
+};
+
+export default Subscriber;
