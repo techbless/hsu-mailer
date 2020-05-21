1315151be07892f8fd5ffcc9d594cbc05e253ea3
techbless <yunbin@hansung.ac.kr>
2020-05-21 15:27:08 +0900

    Add function to send emails

--- src/services/email.ts
+++ src/services/email.ts
@@ -1,9 +1,12 @@
 import * as AWS from 'aws-sdk';
 import * as ejs from 'ejs';
 import Email from '../models/email';
+import TokenService from '../services/token';
 
 class EmailService {
   private ses: AWS.SES;
+
+  private templateLocation: string;
 
   constructor() {
     AWS.config.update({
@@ -13,6 +16,25 @@ class EmailService {
     });
 
     this.ses = new AWS.SES();
+    this.templateLocation = `${__dirname}/../templates`;
+  }
+
+  public findEmailByAddress(email: string) {
+    return Email.findOne({
+      where: {
+        email,
+      },
+    });
+  }
+
+  public async checkSubscription(email: string) {
+    const emailFromDB = await this.findEmailByAddress(email);
+
+    console.log(emailFromDB);
+    if (emailFromDB) {
+      return true;
+    }
+    return false;
   }
 
   public subscribe(email: string) {
@@ -52,6 +74,44 @@ class EmailService {
     return params;
   }
 
+  private async sendEmail(params: AWS.SES.SendEmailRequest) {
+    this.ses.sendEmail(params, (err: AWS.AWSError, data: AWS.SES.SendEmailResponse) => {
+      if (err) console.log(err);
+      else console.log('Email Send -> ', params.Destination.BccAddresses);
+    });
+  }
+
+  public async sendWelcomeEmail(email: string) {
+    const bcc = [email];
+    const subject: string = '비교과 메일링 구독을 마쳤습니다.';
+    const text = '번거로운 비교과 공지 확인, 이제 메일로 빠르게 받아보세요!';
+    const html = await ejs.renderFile(
+      `${this.templateLocation}/welcome.ejs`, {
+        message: text,
+      },
+    );
+
+    const params = this.generateParams(bcc, subject, html, text);
+    this.sendEmail(params);
+  }
+
+  public async sendVerificationEmail(email: Email, purpose: string) {
+    const token = await TokenService.issueToken(email.emailId);
+
+    const bcc = [email.email];
+    const subject = `${purpose}(을)를 위해 이메일 인증을 완료해주세요.`;
+    const verificationUrl = `https://${process.env.SITE_URL}/verify/subscription/${email}/${token}`;
+    const html = await ejs.renderFile(
+      `${this.templateLocation}/verification.ejs`, {
+        purpose,
+        verificationUrl,
+      },
+    );
+
+    const params = this.generateParams(bcc, subject, html, verificationUrl);
+    this.sendEmail(params);
+  }
+
   private distributeEmails(emails: Email[], nEach: number = 30) {
     const nEmails = emails.length;
     const nDistributed = Math.floor(nEmails / nEach);
@@ -66,23 +126,6 @@ class EmailService {
     }
 
     return distributedEmails;
-  }
-
-  public async sendWelcomeEmail(email: Email) {
-    const bcc = [email.email];
-    const subject: string = '비교과 메일링 구독을 마쳤습니다.';
-    const text = '번거로운 비교과 공지 확인, 이제 메일로 빠르게 받아보세요!';
-    const html = await ejs.renderFile(
-      'welcome.ejs', {
-        message: text,
-      },
-    );
-
-    const params = this.generateParams(bcc, subject, html, text);
-    this.ses.sendEmail(params, (err: AWS.AWSError, data: AWS.SES.SendEmailResponse) => {
-      if (err) console.log(err);
-      else console.log('Welcome Email Send -> ', bcc);
-    });
   }
 }
 
