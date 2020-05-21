a4509d9b323c3d07b66d042b4cdb125e20a339f5
techbless <yunbin@hansung.ac.kr>
2020-05-22 00:13:59 +0900

    Add notification entitiy for history and getting latest idx

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,14 +1,17 @@
 import Subscriber, { associate as associateSubscriber } from './subscriber';
 import Token, { associate as associateToken } from './token';
+import Notification, { associate as associateNotification } from './notification';
 
 export * from './sequelize';
 
 const db = {
   Subscriber,
   Token,
+  Notification,
 };
 
 export type dbType = typeof db;
 
 associateSubscriber(db);
 associateToken(db);
+associateNotification(db);
