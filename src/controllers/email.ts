513975a8cd28b3a61d2e18a8d8eaa13e16f9a93e
techbless <yunbin@hansung.ac.kr>
2020-05-21 15:27:48 +0900

    Show subscription result page

--- src/controllers/email.ts
+++ src/controllers/email.ts
@@ -3,13 +3,26 @@ import AsyncHandled from 'express-safe-async';
 
 import EmailService from '../services/email';
 
+
 class EmailController {
   @AsyncHandled
   public async subscribe(req: Request, res: Response) {
     const { email } = req.body;
-    const result = await EmailService.subscribe(email);
 
-    // 인증 메일 발송
+    if (await EmailService.checkSubscription(email)) {
+      return res.render('result', {
+        title: '이미 구독중인 메일입니다.',
+        content: '이미 구독중인 메일 입니다. 다시 확인해주세요.',
+      });
+    }
+
+    const insertedEmail = await EmailService.subscribe(email);
+    await EmailService.sendVerificationEmail(insertedEmail, '구독');
+
+    res.render('result', {
+      title: '메일 인증',
+      content: '인증메일이 발송되었습니다. 메일 인증을 마치면 구독이 완료됩니다.',
+    });
   }
 }
 
