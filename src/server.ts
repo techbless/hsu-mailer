7cfe64317535ec84c9223951bb37cbe4e35f6e19
techbless <yunbin@hansung.ac.kr>
2020-05-22 00:28:50 +0900

    Turn on auto checker when the server starts.

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,16 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+import UpdateChecker from './modules/updateChecker';
 import { sequelize } from './models/index';
 import app from './app';
 
-sequelize.sync()
-  .then(() => {
+sequelize.sync({ force: false })
+  .then(async () => {
+    const updateChecker = new UpdateChecker('http://www.hansung.ac.kr/web/www/1323');
+    await updateChecker.initialize();
+    updateChecker.checkEvery(10); // min
+
     const PORT: number = +process.env.PORT! || 3000;
     app.listen(PORT, (err) => {
       if (err) throw err;
